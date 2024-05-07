a20c35887de313e6df86186ed4a6c7478cdbf35c
Marcos <marcoshr_baptista@hotmail.com>
2024-05-07 14:41:56 -0300

    simplifying api 2

--- Server/src/routes/challenge.js
+++ Server/src/routes/challenge.js
@@ -3,18 +3,42 @@ const router = express.Router();
 
 let tempoProva = "";
 let ProvaLiberada = false;
+let f1 = 0;
+let f2 = 0;
+let f3 = 0;
+let f4 = 0;
+let f5 = 0;
 
 router
     .get('/', (req, res) => {
         res.json({ 
             tempoProva, 
-            ProvaLiberada
+            ProvaLiberada,
+            f1,
+            f2,
+            f3,
+            f4,
+            f5
         });
     })
     .post('/', (req, res) => {
-        const {tempo_Prova, prova_Liberada} = req.body;
+        const {
+            tempo_Prova, 
+            prova_Liberada,
+            f1_,
+            f2_,
+            f3_,
+            f4_,
+            f5_
+        } = req.body;
+
         tempoProva = tempo_Prova;
         ProvaLiberada = prova_Liberada;
+        f1 = f1_;
+        f2 = f2_;
+        f3 = f3_;
+        f4 = f4_;
+        f5 = f5_;
         res.send('Estado da prova atualizado!');
     })
 
--- front/src/components/First/index.js
+++
@@ -1,7 +0,0 @@
-export default function FirstComponent() {
-    return (
-        <p>
-            Meu primeiro componente
-        </p>
-    )
-}
