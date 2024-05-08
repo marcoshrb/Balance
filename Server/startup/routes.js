7de54301ad16b3a281a9e58dc0d1e42171bb3e52
Marcos Henrique <127983473+marcoshrb@users.noreply.github.com>
2024-05-08 15:43:01 -0300

    Delete Server directory

--- Server/startup/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const challenge = require('../src/routes/challenge');
-
-module.exports = function (app) {
-    app.use(express.json());
-    app.use('/challenge', challenge);
-}
